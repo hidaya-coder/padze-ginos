944d80fa50c73ae4ff7746ace55869c0b87422dc
hidaya-coder <hidaya.tabben@etudiant-isi.utm.tn>
2025-11-19 22:17:29 +0100

    fixed Eslint configurations

---
+++ eslint.config.mjs
@@ -0,0 +1,22 @@
+import js from"@elsint.js";
+import globals from "globals";
+import prettier from " eslint-config-prettier";
+
+/** @type {import('eslint').Linter.Config()} */
+
+export default [
+  js.configs.recommended,
+  {
+    files : ["**/*.js", "**/*.jsx"],
+    languageOptions: {
+      globals: {
+        ...globals.browser, ...globals.node},
+      parserOptions: {
+        configuration: {
+          jsx: true,
+        },
+    },
+  },
+},
+  prettier,
+];
--- src/App.js
+++ src/App.js
@@ -1,29 +1,26 @@
 const Pizza = (props) => {
-  return React.createElement("div", {} , [
-    React.createElement("h1" ,{} ,props.name ),
-    React.createElement("p" , {} , props.description ),
-  ] );
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("p", {}, props.description),
+  ]);
 };
 
 const App = () => {
-  return React.createElement(
-    'div', 
-    {},
-    [
-      React.createElement('h1', {}, "Padze Gino's Pizzeria"),
-      React.createElement(Pizza , {
-        name: "Margherita",
-        description: "Classic pizza with tomato sauce, mozzarella, and basil."}),
-      React.createElement(Pizza ,{
-          name : "Pepperoni",
-          description  : "Topped with pepperoni slices and cheese."
-        }),
-      React.createElement(Pizza ,{
-        name : "Veggie Delight",
-        description :" Loaded with bell peppers, onions, mushrooms, and olives."
-      }),
-    ]
-  );
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Padze Gino's Pizzeria"),
+    React.createElement(Pizza, {
+      name: "Margherita",
+      description: "Classic pizza with tomato sauce, mozzarella, and basil.",
+    }),
+    React.createElement(Pizza, {
+      name: "Pepperoni",
+      description: "Topped with pepperoni slices and cheese.",
+    }),
+    React.createElement(Pizza, {
+      name: "Veggie Delight",
+      description: " Loaded with bell peppers, onions, mushrooms, and olives.",
+    }),
+  ]);
 };
 
 const container = document.getElementById("root");

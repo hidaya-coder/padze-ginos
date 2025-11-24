5140efd8da665e429309bb42b63e725cd656d841
hidaya-coder <hidaya.tabben@etudiant-isi.utm.tn>
2025-11-24 20:46:28 +0100

    modified le extention of file app from .js to .jsx(react)

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,19 @@
-import React from 'react';
 import { createRoot } from 'react-dom/client';
 import Pizza from './Pizza.jsx';
 
-const Pizza = (props) => {
-  return React.createElement(
-    "div", {}, [
-    React.createElement("h1", {}, props.name),
-    React.createElement("p", {}, props.description),
-  ]);
-};
-
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Padze Gino's Pizzeria"),
-    React.createElement(Pizza, {
-      name: "Margherita",
-      description: "Classic pizza with tomato sauce, mozzarella, and basil.",
-    }),
-    React.createElement(Pizza, {
-      name: "Pepperoni",
-      description: "Topped with pepperoni slices and cheese.",
-    }),
-    React.createElement(Pizza, {
-      name: "Veggie Delight",
-      description: " Loaded with bell peppers, onions, mushrooms, and olives.",
-    }),
-  ]);
+ return(
+  <div>
+    <x-my-webcomponent></x-my-webcomponent>
+    <input type="text" />
+    <h1>Padze Gino's Pizzeria - Order Now</h1>
+    <Pizza name="Margherita" description="Classic delight with 100% real mozzarella cheese" />
+    <Pizza name="Pepperoni" description="A classic American taste! Relish the delectable flavor of Pepperoni, topped with extra cheese" />
+    <Pizza name="Veggie Supreme" description="Loaded with crunchy onions, crisp capsicum, juicy tomatoes, and jalapeno with extra cheese" />
+  </div>
+ );
 };
 
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(React.createElement(App));
+root.render(<App />);

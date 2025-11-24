b7ac01344aeb9a4a8c88f725b8d0069cfaa13c89
hidaya-coder <hidaya.tabben@etudiant-isi.utm.tn>
2025-11-24 21:54:38 +0100

    set up a Fastify API server with SQLite database

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,17 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-    server: {
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true
+      },
+      '/public' : {
+        target: 'http://localhost:3000',
+        changeOrigin: true
+      }
+    },
     watch: {
       usePolling: true,
     }

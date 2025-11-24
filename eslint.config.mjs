15192e0b1d685b9f648a90a3f7e886c8dac2e5ca
hidaya-coder <hidaya.tabben@etudiant-isi.utm.tn>
2025-11-24 19:41:42 +0100

    completed eslint plugin react configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,22 @@
 import js from"@elsint.js";
 import globals from "globals";
 import prettier from " eslint-config-prettier";
-import reactPlugin from "eslint-plugin-react";
+import react from " eslint-plugin-react";
+import { version } from "vite";
 
 /** @type {import('eslint').Linter.Config()} */
 
 export default [
   js.configs.recommended,
+  {
+    ...reactPlugin.configs.recommended,
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
+  reactPlugin.configs.flat("jsx-runtime"),
   {
     files : ["**/*.js", "**/*.jsx"],
     languageOptions: {
@@ -17,6 +27,11 @@ export default [
           jsx: true,
         },
     },
+  },
+  rules: {
+    "react/no-unescaped-entities": "off",
+    "react/prop-types": "off",
+
   },
 },
   prettier,

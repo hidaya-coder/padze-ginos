bacf754bc457a4e3bed1610f405a4c52b39f7ade
hidaya-coder <hidaya.tabben@etudiant-isi.utm.tn>
2025-11-26 00:41:39 +0100

    added order.jsx file

--- src/Pizza.jsx
+++ src/Pizza.jsx
@@ -1,6 +1,14 @@
+let counter = 0;
 const Pizza = (props) => {
+  counter = Data.now();
+
   return (
-    <div className="pizza">
+    <div
+      className="pizza"
+      onClick = {() => {
+        console.log("hi");
+      }}
+    >
       <h1>{props.name}</h1>
       <p>{props.description}</p>
       <img src={props.image} alt={props.name} />

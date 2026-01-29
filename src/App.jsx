a4364fa6525d98d72bfc9d205c13c459524496dc
hidaya-coder <hidaya.tabben@etudiant-isi.utm.tn>
2026-01-29 11:55:11 +0100

    wraped the components of app  with CartContext

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,23 @@ import { StrictMode } from 'react';
 import { createRoot } from "react-dom/client";
 import Order from "./Order";
 import PizzaOfDay  from "./PizzaOfDay";
+import Header from './Header';
+import { CartContext } from './contexts';
 
 const App = () => {
+  const cartHook = useState([]);
   return (
-    <div>
-      <h1 className="logo">Padze Gino's - Order Now</h1>
-      <Order />
-      <PizzaOfDay />
-    </div>
+    <CartContext.Provider value={cartHook}>
+      <div>
+        <Header />
+        <Order />
+        <PizzaOfDay />
+      </div>
+    </CartContext.Provider>  
   );
 };
+
+/*you can use the hook in the whole app ans set the carthook anywhere with useContext */ 
 
 const container = document.getElementById("root");
 const root = createRoot(container);
---
+++ src/contexts.jsx
@@ -0,0 +1,6 @@
+import { createContext } from "react";
+
+export const CartContext = createContext([[] , function () {}]); 
+
+/* createContext(hook) and const hook = [[] , fuction () {}] */
+
